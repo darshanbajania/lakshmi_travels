51065364926cf87e2435b1ab954fc498367190cf
DARSHAN BAJANIA <darshanbajania1999@gmail.com>
2021-08-11 15:34:56 +0530

    tour packages ui done

--- lakshmi_travels/urls.py
+++ lakshmi_travels/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
+from django.contrib.auth.views import LoginView,LogoutView
+
+
+
+app_name = 'lakshmi_travels'
+
 
 urlpatterns = [
     path('', views.Home, name='home_view'),
+    path('tour_packages/', views.TourPackagesView, name='tour_packages'),
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- lakshmi_travels/views.py
+++ lakshmi_travels/views.py
@@ -6,3 +6,6 @@ from django.shortcuts import render, redirect
 
 def Home(request):
     return render(request, 'lakshmi_travels/index.html')
+
+def TourPackagesView(request):
+    return render(request, 'lakshmi_travels/tour_packages.html')
