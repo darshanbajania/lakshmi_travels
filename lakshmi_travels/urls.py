8ab5134598f82f89fab908b84a73e2dd620d8ca4
DARSHAN BAJANIA <darshanbajania1999@gmail.com>
2021-08-20 02:10:12 +0530

    basic form integration is donw

--- lakshmi_travels/urls.py
+++ lakshmi_travels/urls.py
@@ -15,16 +15,20 @@ urlpatterns = [
     path('tour_packages/', views.TourPackagesView, name='tour_packages'),
     path('daily_trip/', views.DailyTripView, name='daily_trip'),
     path('enquiry/', views.EnquiryPageview, name='enquiry_page'),
+
+
     path('login/', views.LoginView, name='login_page'),
     path('logout/', LogoutView.as_view(next_page='lakshmi_travels:home_view'),name="logout_url"),
     path('register/', views.RegisterView, name='register_page'),
     path('change-password/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'),  name="password_change"),
     path('password-change-done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), name="password_change_done"),
 
+
     path('customer_dashboard/', views.CustomerDashboardView, name='customer_dashboard_page'),
     path('admin_dashboard/', views.AdminDashBoardView, name='admin_dashboard'),
     path('admin_dashboard/customers/', views.AdminDashBoardCustomersView, name='admin_dashboard_customers'),
     path('admin_dashboard/tours/', views.AdminDashBoardToursView, name='admin_dashboard_tours'),
+    path('admin_dashboard/tours/modify', views.AdminDashBoardToursFormView, name='admin_dashboard_tours_form'),
     path('admin_dashboard/rental_cars/', views.AdminDashBoardRentalCarsView, name='admin_dashboard_rental_cars'),
     path('admin_dashboard/daily_trips/', views.AdminDashBoardDailyTripsView, name='admin_dashboard_daily_trips'),
     path('admin_dashboard/daily_trips/modify', views.AdminDashBoardDailyTripsFormView, name='admin_dashboard_daily_trip_form'),
--- lakshmi_travels/views.py
+++ lakshmi_travels/views.py
@@ -1,3 +1,4 @@
+from django.db import router
 from django.http import request
 from django.shortcuts import render, redirect
 from .models import TourPackageDetails, TripDetails, RentalCars, Customers
@@ -12,10 +13,12 @@ from django.urls import reverse
 from urllib.parse import urlencode
 from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
-import datetime
+from datetime import datetime
 from django.core.mail import send_mail
 from django.utils.timezone import utc
+from django.utils import timezone
 from django.http import JsonResponse
+import pytz
 import json
 
 
@@ -23,7 +26,7 @@ import json
 
 
 def Home(request):
-    all_rental_cars = RentalCars.objects.all().filter(availabilty_status = True)
+    all_rental_cars = RentalCars.objects.all().filter(availabilty_status=True)
     all_daily_trips = TripDetails.objects.all()
     all_tour_packages = TourPackageDetails.objects.all()
     all_tour_packages = all_tour_packages[:3]
@@ -39,24 +42,35 @@ def Home(request):
         if booking_request_rental_car_id != None:
             booking_type = 'rental_car'
             base_url = reverse('lakshmi_travels:enquiry_page')
-            query_string = urlencode({'rental_car_id': booking_request_rental_car_id, 'booking_type': booking_type})
+            query_string = urlencode(
+                {'rental_car_id': booking_request_rental_car_id, 'booking_type': booking_type})
             url = '{}?{}'.format(base_url, query_string)
             return redirect(url)
 
         elif trip_booking_request_id != None:
             booking_type = 'daily_trip'
             base_url = reverse('lakshmi_travels:enquiry_page')
-            query_string = urlencode({'daily_trip_id': trip_booking_request_id, 'booking_type': booking_type})
+            query_string = urlencode(
+                {'daily_trip_id': trip_booking_request_id, 'booking_type': booking_type})
             url = '{}?{}'.format(base_url, query_string)
             return redirect(url)
         elif daily_trip_route != None:
             print(daily_trip_route)
             base_url = reverse('lakshmi_travels:daily_trip')
-            query_string = urlencode({'daily_trip_route': daily_trip_route, 'daily_trip_date': daily_trip_date})
+            query_string = urlencode(
+                {'daily_trip_route': daily_trip_route, 'daily_trip_date': daily_trip_date})
             url = '{}?{}'.format(base_url, query_string)
             return redirect(url)
-        # print(url)
 
+        if request.POST.get('tour_request_type') == 'details':
+            tour_package_id = request.POST.get('tour_id')
+            base_url = reverse('lakshmi_travels:login_page')
+            query_string = urlencode(
+                {'daily_trip_route': daily_trip_route, 'daily_trip_date': daily_trip_date})
+            url = '{}?{}'.format(base_url, query_string)
+            return redirect(url)
+
+        # print(url)
 
     context = {
         'all_rental_cars': all_rental_cars,
@@ -67,6 +81,7 @@ def Home(request):
 
     return render(request, 'lakshmi_travels/index.html', context)
 
+
 def TourPackagesView(request):
     all_tour_packages = TourPackageDetails.objects.all()
     context = {
@@ -74,10 +89,12 @@ def TourPackagesView(request):
     }
     return render(request, 'lakshmi_travels/tour_packages.html', context)
 
+
 def DailyTripView(request):
     daily_trip_route = request.GET.get('daily_trip_route')
     daily_trip_date = request.GET.get('daily_trip_date')
-    selected_day_trip = TripDetails.objects.all().filter(route = daily_trip_route, trip_timings__date = daily_trip_date)
+    selected_day_trip = TripDetails.objects.all().filter(
+        route=daily_trip_route, trip_timings__date=daily_trip_date)
 
     if request.method == "POST":
         trip_booking_request_id = request.POST.get('trip_id')
@@ -86,22 +103,25 @@ def DailyTripView(request):
         if trip_booking_request_id != None:
             booking_type = 'daily_trip'
             base_url = reverse('lakshmi_travels:enquiry_page')
-            query_string = urlencode({'daily_trip_id': trip_booking_request_id, 'booking_type': booking_type})
+            query_string = urlencode(
+                {'daily_trip_id': trip_booking_request_id, 'booking_type': booking_type})
             url = '{}?{}'.format(base_url, query_string)
             return redirect(url)
         elif daily_trip_route != None:
             print(daily_trip_route)
             base_url = reverse('lakshmi_travels:daily_trip')
-            query_string = urlencode({'daily_trip_route': daily_trip_route, 'daily_trip_date': daily_trip_date})
+            query_string = urlencode(
+                {'daily_trip_route': daily_trip_route, 'daily_trip_date': daily_trip_date})
             url = '{}?{}'.format(base_url, query_string)
-            return redirect(url)    
+            return redirect(url)
     context = {
         'selected_day_trip': selected_day_trip,
     }
-    
+
     print(selected_day_trip)
 
     return render(request, 'lakshmi_travels/daily_trip.html', context)
+
 
 def EnquiryPageview(request):
     booking_type = request.GET.get('booking_type')
@@ -110,22 +130,22 @@ def EnquiryPageview(request):
     selected_car = None
     selected_trip = None
     # if request.method == "POST":
-        
-        # name = request.POST.get('name')
-        # sender = request.POST.get('email_id')
-        # mobile = request.POST.get('phone')
-        # query = request.POST.get('message')
-        # recipients = ['darshanbajania1999@gmail.com']
-        # subject = "Website Inquiry"
-        # message = "hello testing"
-        # message = "Name: "+name + "\nEmail Id: "+ sender + "\nMobile No: "+ str(mobile) +"\nMessage: " + query
-        # send_mail(subject, message, 'darshanbajania1999@gmail.com', recipients)
+
+    # name = request.POST.get('name')
+    # sender = request.POST.get('email_id')
+    # mobile = request.POST.get('phone')
+    # query = request.POST.get('message')
+    # recipients = ['darshanbajania1999@gmail.com']
+    # subject = "Website Inquiry"
+    # message = "hello testing"
+    # message = "Name: "+name + "\nEmail Id: "+ sender + "\nMobile No: "+ str(mobile) +"\nMessage: " + query
+    # send_mail(subject, message, 'darshanbajania1999@gmail.com', recipients)
     if booking_type == 'rental_car':
-        selected_car = RentalCars.objects.all().filter(car_id = car_request_id).first()
-        if selected_car!= None:
+        selected_car = RentalCars.objects.all().filter(car_id=car_request_id).first()
+        if selected_car != None:
             booking_type = 'rental_car'
     elif booking_type == 'daily_trip':
-        selected_trip = TripDetails.objects.all().filter(trip_id = daily_trip_id).first()
+        selected_trip = TripDetails.objects.all().filter(trip_id=daily_trip_id).first()
 
     # print(booking_type, car_request_id, daily_trip_id)
     context = {
@@ -134,6 +154,7 @@ def EnquiryPageview(request):
         'selected_trip': selected_trip,
     }
     return render(request, 'lakshmi_travels/enquiry_page.html', context)
+
 
 def LoginView(request):
     if request.session.has_key('is_logged_in'):
@@ -144,15 +165,17 @@ def LoginView(request):
             login_password = request.POST.get('password')
             # print(login_username, login_password)
 
-            user = authenticate(username=login_username, password=login_password)
+            user = authenticate(username=login_username,
+                                password=login_password)
             print(user)
 
             if user is not None:
                 login(request, user)
                 request.session['is_logged_in'] = True
-                return redirect('/')        
+                return redirect('/')
 
     return render(request, 'registration/login.html')
+
 
 def RegisterView(request):
     form = CreateUserForm()
@@ -166,13 +189,16 @@ def RegisterView(request):
     }
     return render(request, 'registration/register.html', context)
 
+
 @login_required
 def CustomerDashboardView(request):
     return render(request, 'lakshmi_travels/customer_dashboard.html')
 
+
 @login_required
 def AdminDashBoardView(request):
     return render(request, 'lakshmi_travels/admin_dashboard.html')
+
 
 @login_required
 def AdminDashBoardCustomersView(request):
@@ -183,9 +209,63 @@ def AdminDashBoardCustomersView(request):
     print(all_users)
     return render(request, 'lakshmi_travels/admin_dashboard_customers.html', context)
 
+
 @login_required
 def AdminDashBoardToursView(request):
-    return render(request, 'lakshmi_travels/admin_dashboard_tours.html')
+    all_tours = TourPackageDetails.objects.all()
+    if request.method == "POST":
+        change_type = request.POST.get('change_type')
+
+        if change_type == 'add_new':
+            return redirect('lakshmi_travels:admin_dashboard_tours_form')
+        if change_type == 'delete_tour':
+            tour_id = request.POST.get('delete_tour_id')
+            print(tour_id)
+            if tour_id is not None:
+                sel_tour = TourPackageDetails.objects.all().filter(tour_id=tour_id).first()
+                sel_tour.delete()
+
+    context = {
+        'all_tours': all_tours,
+    }
+
+    return render(request, 'lakshmi_travels/admin_dashboard_tours.html', context)
+
+
+@login_required
+def AdminDashBoardToursFormView(request):
+    all_tours = TourPackageDetails.objects.all()
+    # IST = pytz.timezone('Asia/Kolkata')
+    # print(datetime.now(IST))
+    if request.method == 'POST':
+        tour_name = request.POST.get('tour_name')
+        tour_summary = request.POST.get('tour_summary')
+        tour_duration = request.POST.get('tour_duration')
+        tour_image = request.FILES['tour_image']
+        tour_details = request.POST.get('tour_details')
+
+        print(tour_name, tour_summary, tour_duration, tour_image, tour_details)
+        if tour_name is not None and tour_summary is not None and tour_duration is not None and tour_image is not None and tour_details is not None:
+            latest_id = TourPackageDetails.objects.all().order_by(
+                'tour_id').reverse().first().id
+            print(latest_id)
+            new_tour = TourPackageDetails(tour_id=latest_id + 1, 
+                                    package_name=tour_name, 
+                                    summary=tour_summary,
+                                    duration=tour_duration, 
+                                    details=tour_details, 
+                                    tour_package_image=tour_image, 
+                                    availability_status=True)
+            new_tour.save()
+
+    td_form = TripDetailsForm()
+    context = {
+        'td_form': td_form,
+        'all_tours': all_tours,
+    }
+
+    return render(request, 'lakshmi_travels/admin_dashboard_tours_form.html', context)
+
 
 @login_required
 def AdminDashBoardRentalCarsView(request):
@@ -193,17 +273,17 @@ def AdminDashBoardRentalCarsView(request):
     counter = 1
     if request.method == 'POST':
         for rental_car in all_rental_cars:
-            
+
             car_data = request.POST.get('car_'+str(rental_car.car_id))
             if car_data == 'on':
                 rental_car.availabilty_status = True
             else:
                 rental_car.availabilty_status = False
-            
+
             rental_car.save()
             print(rental_car.availabilty_status)
 
-            # elif car_data == 
+            # elif car_data ==
             # print(car_data)
             counter += 1
         car_data = request.POST
@@ -212,13 +292,24 @@ def AdminDashBoardRentalCarsView(request):
         'all_rental_cars': all_rental_cars,
     }
     return render(request, 'lakshmi_travels/admin_dashboard_rental_cars.html', context)
+
+
 @login_required
 def AdminDashBoardDailyTripsView(request):
     all_daily_trips = TripDetails.objects.all()
     if request.method == "POST":
         change_type = request.POST.get('change_type')
+        trip_id = request.POST.get('trip_price')
+
         if change_type == 'add_new':
             return redirect('lakshmi_travels:admin_dashboard_daily_trip_form')
+        if change_type == 'delete_trip':
+            trip_id = request.POST.get('delete_trip_id')
+            print(trip_id)
+            if trip_id is not None:
+                sel_trip = TripDetails.objects.all().filter(trip_id=trip_id).first()
+                sel_trip.delete()
+
     context = {
         'all_daily_trips': all_daily_trips,
     }
@@ -229,10 +320,24 @@ def AdminDashBoardDailyTripsView(request):
 @login_required
 def AdminDashBoardDailyTripsFormView(request):
     all_daily_trips = TripDetails.objects.all()
+    IST = pytz.timezone('Asia/Kolkata')
+    # print(datetime.now(IST))
     if request.method == 'POST':
-        trip_route = request.POST.get('car_route')
-        trip_date = request.POST.get('car_date')
-        trip_price = request.POST.get('car_price')
+        trip_route = request.POST.get('trip_route')
+        trip_date = request.POST.get('trip_date')
+        trip_price = request.POST.get('trip_price')
+        # print(trip_date)
+        trip_date = datetime.strptime(trip_date, "%Y-%m-%dT%H:%M")
+        # print(trip_date)
+        trip_date = timezone.make_aware(trip_date, timezone=IST)
+        # print(trip_route, trip_price, trip_date)
+
+        if trip_route is not None and trip_date is not None and trip_price is not None:
+            latest_id = TripDetails.objects.all().order_by('trip_id').reverse().first().id
+            # print(latest_id)
+            new_trip = TripDetails(trip_id=latest_id + 1,
+                                   route=trip_route, trip_timings=trip_date)
+            new_trip.save()
 
     td_form = TripDetailsForm()
     context = {
